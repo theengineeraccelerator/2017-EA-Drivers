9b35b49e5989912806effcff2983aa3d34379b60
Malcolm Knapp <engineeraccelerator@gmail.com>
2017-09-16 18:02:11 -0700

    added stdint.h include and cast

--- MAX17055/Negitive Numbers and Constructors/Twos_Complement_BitMath/Twos_Complement_BitMath.ino
+++ MAX17055/Negitive Numbers and Constructors/Twos_Complement_BitMath/Twos_Complement_BitMath.ino
@@ -3,21 +3,32 @@
  *  Project: 2's Complement Example
  * 
  */
+#include <stdint.h>
 
 unsigned int registerHex = 0xFFFF;
+uint16_t  u_registerHex = 0xFFFF;
 int negative_value;
 int postive_value;
 
 void setup() {
   Serial.begin (115200);
+  
   Serial.println ("1.8.4");
-  Serial.println ("'Negative' Hex");
-  Serial.print ("Uninterpreted register Hex "); Serial.println (registerHex);
-  Serial.print ("Uninterpreted register Hex in Binary: "); Serial.println (registerHex, BIN);
+  Serial.println ("'Negative' uint16_t");
+  Serial.print ("Uninterpreted register uint16_t "); Serial.println (u_registerHex);
+  Serial.print ("Uninterpreted register uint16_t in Binary: "); Serial.println (u_registerHex, BIN);
   // cast to a signed interpretation
-  Serial.print ("Int cast register Hex "); Serial.println ((int)registerHex);
-  Serial.print ("Interpreted register Hex "); Serial.println ((signed)registerHex);
-  Serial.print ("Interpreted register Hex in Binary: "); Serial.println (registerHex, BIN);
+  Serial.print ("int16_t cast register to int16_t "); Serial.println ((int16_t)u_registerHex);
+  Serial.print ("int16_t cast register to int16_t in Binary: "); Serial.println ((int16_t)u_registerHex, BIN);
+  Serial.println("");
+  
+  Serial.println ("'Negative' unsigned int");
+  Serial.print ("Uninterpreted register unsigned int "); Serial.println (registerHex);
+  Serial.print ("Uninterpreted register unsigned int in Binary: "); Serial.println (registerHex, BIN);
+  // cast to a signed interpretation
+  Serial.print ("Int cast register to signed int "); Serial.println ((int)registerHex);
+  Serial.print ("register cast to signed "); Serial.println ((signed)registerHex);
+  Serial.print ("Cast register Hex in Binary: "); Serial.println ((signed)registerHex, BIN);
   Serial.println("");
   
   registerHex = 0x0FFF;
